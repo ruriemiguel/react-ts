817bcb26d145f4c1262137328fc6fd452dd1dd8e
Ruriê <rurie.miguel@gmail.com>
2022-09-28 23:58:02 -0300

    Alterações Aula 28/09

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1>Home</h1>
+            <p>Teste 2</p>
+        </>
+    );
+}
+
+export default Home;
