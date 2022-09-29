54085e23ae140fcf7cd22f11d8bd6062d50c3aef
Ruriê <rurie.miguel@gmail.com>
2022-09-28 23:59:54 -0300

    correção

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -4,7 +4,7 @@ import './Home.css';
 function Home(){
     return (
         <>
-            <h1>Home</h1>
+            <h1 className="titulo">Home</h1>
             <p>Teste 2</p>
         </>
     );
